acada60216ce95e7b1ed9a6125115e86772823f7
vivek <ikumarvivek786@gmail.com>
2023-04-17 15:15:45 +0530

    [Vivek]Constructor,Access Modifiers & Interface]

---
+++ Practice problem Day 3B/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_problem_Day_3B
+{
+    public class Person
+    {
+
+        private string name;
+        private int age;
+
+        // default constructor
+        public Person()
+        {
+            name = "";
+            age = 0;
+        }
+
+        // parameterized constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
